0cf57b197dd7384c988a21372104812435939d78
sai <sailinnthu@gmail.com>
2024-09-29 00:22:40 +0800

    fix project-name

--- 0-workspace-controller/main.tf
+++ 0-workspace-controller/main.tf
@@ -2,19 +2,19 @@ resource "tfe_workspace" "hcp-boundary-cluster-ws" {
   name         = "1-hcp-boundary-cluster-ws"
   organization = data.tfe_organization.hellocloud-tfc.name
   tag_names    = ["hcp-boundary-cluster", "app"]
-  project_id   = data.tfe_project.default-project.id
+  project_id   = data.tfe_project.boundary-multihop-project.id
 }
 
 resource "tfe_workspace" "ingress-worker-ws" {
   name         = "2-ingress-worker-ws"
   organization = data.tfe_organization.hellocloud-tfc.name
   tag_names    = ["ingress-worker", "app"]
-  project_id   = data.tfe_project.default-project.id
+  project_id   = data.tfe_project.boundary-multihop-project.id
 }
 
 resource "tfe_workspace" "egress-worker-ws" {
   name         = "3-egress-worker-ws"
   organization = data.tfe_organization.hellocloud-tfc.name
   tag_names    = ["egress-worker", "app"]
-  project_id   = data.tfe_project.default-project.id
+  project_id   = data.tfe_project.boundary-multihop-project.id
 }
