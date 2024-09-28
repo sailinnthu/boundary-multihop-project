40150f0427262f428428dbfc6548af02a6d2fee9
sai <sailinnthu@gmail.com>
2024-09-29 01:35:50 +0800

    test destroy ws

--- 0-workspace-controller/main.tf
+++ 0-workspace-controller/main.tf
@@ -13,62 +13,62 @@ resource "tfe_workspace" "hcp-boundary-cluster-ws" {
   queue_all_runs = false
 }
 
-resource "tfe_workspace" "aws-boundary-infra-ws" {
-  name         = "2-aws-boundary-infra-ws"
-  organization = data.tfe_organization.hellocloud-tfc.name
-  tag_names    = ["aws-boundary-infra", "app"]
-  project_id   = data.tfe_project.boundary-multihop-project.id
-  vcs_repo {
-    branch             = "main"
-    identifier         = "sailinnthu/boundary-multihop-project"
-    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
-  }
-  working_directory = "2-aws-boundary-infra-ws"
-  trigger_patterns = []
-  queue_all_runs = false
-}
+# resource "tfe_workspace" "aws-boundary-infra-ws" {
+#   name         = "2-aws-boundary-infra-ws"
+#   organization = data.tfe_organization.hellocloud-tfc.name
+#   tag_names    = ["aws-boundary-infra", "app"]
+#   project_id   = data.tfe_project.boundary-multihop-project.id
+#   vcs_repo {
+#     branch             = "main"
+#     identifier         = "sailinnthu/boundary-multihop-project"
+#     oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+#   }
+#   working_directory = "2-aws-boundary-infra-ws"
+#   trigger_patterns = []
+#   queue_all_runs = false
+# }
 
-resource "tfe_workspace" "ingress-worker-ws" {
-  name         = "3-ingress-worker-ws"
-  organization = data.tfe_organization.hellocloud-tfc.name
-  tag_names    = ["ingress-worker", "app"]
-  project_id   = data.tfe_project.boundary-multihop-project.id
-  vcs_repo {
-    branch             = "main"
-    identifier         = "sailinnthu/boundary-multihop-project"
-    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
-  }
-  working_directory = "3-ingress-worker-ws"
-  trigger_patterns = []
-  queue_all_runs = false
-}
+# resource "tfe_workspace" "ingress-worker-ws" {
+#   name         = "3-ingress-worker-ws"
+#   organization = data.tfe_organization.hellocloud-tfc.name
+#   tag_names    = ["ingress-worker", "app"]
+#   project_id   = data.tfe_project.boundary-multihop-project.id
+#   vcs_repo {
+#     branch             = "main"
+#     identifier         = "sailinnthu/boundary-multihop-project"
+#     oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+#   }
+#   working_directory = "3-ingress-worker-ws"
+#   trigger_patterns = []
+#   queue_all_runs = false
+# }
 
-resource "tfe_workspace" "egress-worker-ws" {
-  name         = "4-egress-worker-ws"
-  organization = data.tfe_organization.hellocloud-tfc.name
-  tag_names    = ["egress-worker", "app"]
-  project_id   = data.tfe_project.boundary-multihop-project.id
-  vcs_repo {
-    branch             = "main"
-    identifier         = "sailinnthu/boundary-multihop-project"
-    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
-  }
-  working_directory = "4-egress-worker-ws"
-  trigger_patterns = []
-  queue_all_runs = false
-}
+# resource "tfe_workspace" "egress-worker-ws" {
+#   name         = "4-egress-worker-ws"
+#   organization = data.tfe_organization.hellocloud-tfc.name
+#   tag_names    = ["egress-worker", "app"]
+#   project_id   = data.tfe_project.boundary-multihop-project.id
+#   vcs_repo {
+#     branch             = "main"
+#     identifier         = "sailinnthu/boundary-multihop-project"
+#     oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+#   }
+#   working_directory = "4-egress-worker-ws"
+#   trigger_patterns = []
+#   queue_all_runs = false
+# }
 
-resource "tfe_workspace" "dev-target1-ws" {
-  name         = "5-dev-target1-ws"
-  organization = data.tfe_organization.hellocloud-tfc.name
-  tag_names    = ["dev-target1", "app"]
-  project_id   = data.tfe_project.boundary-multihop-project.id
-  vcs_repo {
-    branch             = "main"
-    identifier         = "sailinnthu/boundary-multihop-project"
-    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
-  }
-  working_directory = "5-dev-target1-ws"
-  trigger_patterns = []
-  queue_all_runs = false
-}
+# resource "tfe_workspace" "dev-target1-ws" {
+#   name         = "5-dev-target1-ws"
+#   organization = data.tfe_organization.hellocloud-tfc.name
+#   tag_names    = ["dev-target1", "app"]
+#   project_id   = data.tfe_project.boundary-multihop-project.id
+#   vcs_repo {
+#     branch             = "main"
+#     identifier         = "sailinnthu/boundary-multihop-project"
+#     oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+#   }
+#   working_directory = "5-dev-target1-ws"
+#   trigger_patterns = []
+#   queue_all_runs = false
+# }
