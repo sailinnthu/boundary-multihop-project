b1c8114469ca03ca929d4edb63f3380b4e0b0975
sai <sailinnthu@gmail.com>
2024-09-29 01:38:19 +0800

    test create ws

--- 0-workspace-controller/outputs.tf
+++ 0-workspace-controller/outputs.tf
@@ -6,14 +6,14 @@ output "HCP-Boundary-WS" {
   value = tfe_workspace.hcp-boundary-cluster-ws.name
 }
 
-# output "AWS-Boundary-Infra-WS" {
-#   value = tfe_workspace.aws-boundary-infra-ws.name
-# }
+output "AWS-Boundary-Infra-WS" {
+  value = tfe_workspace.aws-boundary-infra-ws.name
+}
 
-# output "Ingress-Worker-WS" {
-#   value = tfe_workspace.ingress-worker-ws.name
-# }
+output "Ingress-Worker-WS" {
+  value = tfe_workspace.ingress-worker-ws.name
+}
 
-# output "Egress-Worker-WS" {
-#   value = tfe_workspace.egress-worker-ws.name
-# }
+output "Egress-Worker-WS" {
+  value = tfe_workspace.egress-worker-ws.name
+}
